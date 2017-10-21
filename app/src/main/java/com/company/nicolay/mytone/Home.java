e0af438a074f3cb665e21042d8e993377731ea1a
Nicolay <Nicolay.JG@outlook.com>
2017-10-21 10:53:17 -0500

    Aleatorio Funcionando
    Se implemento la función de generar un aleatorio

--- app/src/main/java/com/company/nicolay/mytone/Home.java
+++ app/src/main/java/com/company/nicolay/mytone/Home.java
@@ -2,12 +2,35 @@ package com.company.nicolay.mytone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+import java.util.Random;
+
+public class Home extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button button = (Button)findViewById(R.id.BtR);
+        button.setOnClickListener(this);
     }
+
+
+    private int myRandom(){
+        Random m = new Random();
+        return m.nextInt(10)+1;
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView text = (TextView) findViewById(R.id.Dev);
+        String value = String.valueOf(myRandom());
+        Log.i("Nico!!!!!!->",value);
+        text.setText(value);
+    }
+
 }
